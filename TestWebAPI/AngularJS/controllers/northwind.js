7ac24a7e595636f4daa911ed63b27127bc8595b6
avi0119 <avisemah@gmail.com>
2015-05-28 22:39:03 -0400

    added  more amgular comtrollers and filters

---
+++ TestWebAPI/AngularJS/controllers/northwind.js
@@ -0,0 +1,18 @@
+﻿angular.module("northWind").controller('northwindCtrl',
+function ($scope, $http) {
+    $scope.data = {};
+
+    $http({
+        method: 'GET',
+        url: "http://localhost:39402/api/product?" + 'callback=JSON_CALLBACK'
+    }).success(function (data, status) {
+        $scope.data.products = data;
+        var i;
+        for (i = 0; i < data.length; ++i) {
+            console.log(data[i]);                    // Do something with `substr[i]`
+        }
+
+    }).error(function (data, status) {
+        // Some error occurred
+    });
+});
---
+++ TestWebAPI/AngularJS/controllers/productListControllers.js
@@ -0,0 +1,26 @@
+﻿angular.module("northWind")
+.constant("productListActiveClass", "btn-primary")
+.constant("productListPageCount", 6)
+.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount)
+{
+    $scope.selectedPage = 1;
+    $scope.pageSize = productListPageCount;
+    var selectedCategory = null;
+    $scope.selectCategory = function (newCategory) {
+        selectedCategory = newCategory;
+        $scope.selectedPage = 1;
+    }
+    $scope.selectPage = function (newPage) {
+        $scope.selectedPage = newPage;
+    }
+    $scope.categoryFilterFn = function (product) {
+        return selectedCategory == null ||
+        product.Category.CategoryName == selectedCategory;
+    }
+    $scope.getCategoryClass = function (category) {
+        return selectedCategory == category ? productListActiveClass : "";
+    }
+    $scope.getPageClass = function (page) {
+        return $scope.selectedPage == page ? productListActiveClass : "";
+    }
+});
